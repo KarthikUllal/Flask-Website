bcc230705d7a88ad323e3b13f8b8ed5b554f9e4c
KarthikUllal <karthik2003ku@gmail.com>
2025-07-23 16:21:24 +0000

    Added About route

--- app.py
+++ app.py
@@ -13,5 +13,10 @@ def hello():
   return "<p>Hello, World!</p>"
 
 
+@app.route("/about/")
+def about():
+  return "<p>This is a About Page</p>"
+
+
 if __name__ == "__main__":
   app.run(debug=True, host="0.0.0.0")
