213648b518ffd023138042b2acc3d82aa0bc7dc7
KarthikUllal <karthik2003ku@gmail.com>
2025-07-23 16:11:11 +0000

    added one extra route

--- app.py
+++ app.py
@@ -1,10 +1,17 @@
 from flask import Flask
+
 app = Flask(__name__)
 
+
 @app.route("/")
-def hello_world(): 
+def hello_world():
   return "Hello, Karthik!"
 
+
+@app.route("/hello/")
+def hello():
+  return "<p>Hello, World!</p>"
+
+
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0")
-  
+  app.run(debug=True, host="0.0.0.0")
